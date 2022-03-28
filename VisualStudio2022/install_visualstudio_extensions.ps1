a61055b02eab036167a4b03804cf74072d63f926
Jacques Gariépy <jacques.gariepy@outlook.com>
2022-03-28 19:55:45 -0400

    add extensions

--- VisualStudio2022/install_visualstudio_extensions.ps1
+++ VisualStudio2022/install_visualstudio_extensions.ps1
@@ -1,3 +1,15 @@
-﻿$DownloadAndInstall= $PSScriptRoot+"\_install_script.ps1"
-& $DownloadAndInstall -PackageName "ErikEJ.EFCorePowerTools" "/q /a"
+$DownloadAndInstall= $PSScriptRoot+"\_install_script.ps1"
 
+& $DownloadAndInstall -PackageName "MadsKristensen.NpmTaskRunner64" "/q /a"
+& $DownloadAndInstall -PackageName "ErikEJ.SQLServerCompactSQLiteToolbox" "/q /a"
+& $DownloadAndInstall -PackageName "ErikEJ.EFCorePowerTools" "/q /a"
+& $DownloadAndInstall -PackageName "SteveCadwallader.CodeMaidVS2022" "/q /a"
+& $DownloadAndInstall -PackageName "Mojtabakaviani.SqlTools" "/q /a"
+& $DownloadAndInstall -PackageName "EWoodruff.VisualStudioSpellCheckerVS2022andLater" "/q /a"
+& $DownloadAndInstall -PackageName "WixToolset.WixToolsetVisualStudio2022Extension" "/a"
+& $DownloadAndInstall -PackageName "ProBITools.MicrosoftReportProjectsforVisualStudio2022" "/q /a"
+& $DownloadAndInstall -PackageName "DevartSoftware.CodeCompare" "/q /a"
+& $DownloadAndInstall -PackageName "VisualStudioPlatformTeam.ProductivityPowerPack2022" "/q /a"
+
+Write-Host "Installation dotnet nuget package" -ForegroundColor Yellow
+dotnet tool install --global Microsoft.Tye --version 0.11.0-alpha.22111.1
