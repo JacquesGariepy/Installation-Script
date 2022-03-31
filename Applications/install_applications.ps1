3c6822c569fb7bd702d90f00b4e94c22e8ace216
jacques gariepy <jacques.gariepy@hotmail.com>
2022-03-30 23:59:46 -0400

    changement script

---
+++ Applications/install_applications.ps1
@@ -0,0 +1,59 @@
+# Terminal
+winget install -e -h --id Microsoft.PowerShell.Preview --force --accept-source-agreements
+winget install -e -h --id Microsoft.WindowsTerminal.Preview --force --accept-source-agreements
+winget install -e -h --id JanDeDobbeleer.OhMyPosh --force --accept-source-agreements
+
+# Browsers
+winget install -e -h --id Google.Chrome --force --accept-source-agreements
+winget install -e -h --id Google.Chrome.Dev --force --accept-source-agreements
+winget install -e -h --id Google.Chrome.Beta --force --accept-source-agreements
+winget install -e -h --id Mozilla.Firefox --force --accept-source-agreements
+winget install -e -h --id Mozilla.Firefox.DeveloperEdition --force --accept-source-agreements
+winget install -e -h --id Microsoft.Edge.Beta --force --accept-source-agreements
+
+# IDEs
+winget install -e -i --id Microsoft.VisualStudio.2022.Enterprise-Preview --force --accept-source-agreements
+winget install -e -h --id Microsoft.VisualStudioCode --force --accept-source-agreements
+winget install -e -h --id Microsoft.VisualStudioCode.Insiders --force --accept-source-agreements
+winget install -e -h --id Notepad++.Notepad++ --force --accept-source-agreements
+
+# Frameworks
+winget install -e -h --id Microsoft.dotnet --force --accept-source-agreements
+winget install -e -h --id OpenJS.NodeJS --force --accept-source-agreements
+winget install -e -h --id Yarn.Yarn --force --accept-source-agreements
+
+# dev
+winget install -e -h --id Python.Python.3 --force --accept-source-agreements
+
+# Git
+winget install -e -i --id Git.Git --force --accept-source-agreements
+winget install -e -h --id GitHub.GitLFS --force --accept-source-agreements
+winget install -e -h --id GitHub.cli --force --accept-source-agreements
+winget install -e -h --id Axosoft.GitKraken --force --accept-source-agreements
+
+#db
+winget install -e -i --id Microsoft.AzureDataStudio.Insiders --force --accept-source-agreements
+winget install -e -i --id Microsoft.SQLServer.2019.Developer --force --accept-source-agreements
+winget install -e -i --id Microsoft.SQLServerManagementStudio --force --accept-source-agreements
+
+# Tools
+winget install -e -h --id Docker.DockerDesktop --force --accept-source-agreements
+winget install -e -h --id Microsoft.PowerBI  --force --accept-source-agreements
+winget install -e -h --id Telerik.Fiddler --force --accept-source-agreements
+winget install -e -h --id JetBrains.ReSharper --force --accept-source-agreements
+winget install -e -h --id JetBrains.Rider --force --accept-source-agreements
+winget install -e -h --id JetBrains.WebStorm --force --accept-source-agreements
+winget install -e -h --id JetBrains.DataGrip --force --accept-source-agreements
+
+# Azure
+winget install -e -h --id Microsoft.AzureDataStudio --force --accept-source-agreements
+winget install -e -h --id Microsoft.AzureStorageExplorer --force --accept-source-agreements
+winget install -e -h --id Microsoft.AzureCLI --force --accept-source-agreements
+winget install -e -h --id Microsoft.AzureCosmosEmulator --force --accept-source-agreements
+winget install -e -h --id Microsoft.AzureStorageEmulator --force --accept-source-agreements
+
+# Utilities
+winget install -e -h --id 7zip.7zip --force --accept-source-agreements
+winget install -e -h --id Adobe.Acrobat.Reader.64-bit --force --accept-source-agreements
+winget install -e -h --id Microsoft.PowerToys --force --accept-source-agreements
+winget install -e -h --id WinSCP.WinSCP --force --accept-source-agreements
