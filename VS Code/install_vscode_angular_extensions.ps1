9062f9ca160fb2cd90bc429a537ce2e8246c056a
Jacques Gariépy <jacques.gariepy@outlook.com>
2023-09-16 20:45:26 -0400

    Update install_vscode_angular_extensions.ps1

--- VS Code/install_vscode_angular_extensions.ps1
+++ VS Code/install_vscode_angular_extensions.ps1
@@ -1,67 +1,64 @@
-
 [CmdletBinding()]
 param(
-    [parameter()]
-    [ValidateSet(,"64-bit","32-bit")]
+    [Parameter()]
+    [ValidateSet("64-bit", "32-bit")]
     [string]$Architecture = "64-bit",
 
-    [parameter()]
-    [ValidateSet("stable","insider")]
-    
-    #stable ou insider
+    [Parameter()]
+    [ValidateSet("stable", "insider")]
     [string]$BuildEdition = "insider",
-    
+
     [Parameter()]
     [ValidateNotNull()]
     [string[]]$AdditionalExtensions = @(
-    "aaron-bond.better-comments",
-	  "Angular.ng-template",
-	  "johnpapa.angular-essentials",
-	  "johnpapa.Angular2",
+        "aaron-bond.better-comments",
+        "Angular.ng-template",
+        "johnpapa.angular-essentials",
+        "johnpapa.Angular2"
+    ),
+
+    [Parameter()]
     [switch]$LaunchWhenDone
 )
-	#path d'installation de vscode
-    $codePath = "C:\Users\jgariepy\AppData\Local\Programs\"
-    $bitVersion = "win32-x64"
 
-    try {
-        $ProgressPreference = 'SilentlyContinue'
+# Installation path for VSCode
+$codePath = "C:\Users\jgariepy\AppData\Local\Programs\"
 
-        #version des vscode
-		$buildEditions = @("stable", "insider")
+# Determine bit version based on architecture
+$bitVersion = if ($Architecture -eq "64-bit") { "win32-x64" } else { "win32-x86" }
 
-        foreach ($buildEdition in $buildEditions) {
+try {
+    $ProgressPreference = 'SilentlyContinue'
 
-            switch ($buildEdition) {
-                "Stable" {
-                    $codeCmdPath = "$codePath\Microsoft VS Code\bin\code.cmd"
-                    $appName = "Visual Studio Code ($($Architecture))"
-
-                    break;
-                }
-                "Insider" {
-                    $codeCmdPath = "$codePath\Microsoft VS Code Insiders\bin\code-insiders.cmd"
-                    $appName = "Visual Studio Code - Insiders Edition ($($Architecture))"
-                    break;
-                }
-            }
-            Write-Host "`nInstalling for $buildEdition..." -ForegroundColor Green
-            
-            $extensions = @("ms-vscode.PowerShell") + $AdditionalExtensions
-            foreach ($extension in $extensions) {
-                Write-Host "`nInstalling extension $extension" -ForegroundColor Yellow
-                & $codeCmdPath --install-extension $extension
-            }
-
-            if ($LaunchWhenDone) {
-                Write-Host "`nInstallation complete, starting $appName...`n`n" -ForegroundColor Green
-                & $codeCmdPath
-            }
-            else {
-                Write-Host "`nInstallation complete!`n`n" -ForegroundColor Green
-            }
+    switch ($BuildEdition) {
+        "stable" {
+            $codeCmdPath = "$codePath\Microsoft VS Code\bin\code.cmd"
+            $appName = "Visual Studio Code ($Architecture)"
+        }
+        "insider" {
+            $codeCmdPath = "$codePath\Microsoft VS Code Insiders\bin\code-insiders.cmd"
+            $appName = "Visual Studio Code - Insiders Edition ($Architecture)"
         }
     }
-    finally {
-        $ProgressPreference = 'Continue'
+
+    Write-Host "`nInstalling for $BuildEdition..." -ForegroundColor Green
+
+    # Combine default and additional extensions
+    $extensions = @("ms-vscode.PowerShell") + $AdditionalExtensions
+
+    foreach ($extension in $extensions) {
+        Write-Host "`nInstalling extension $extension" -ForegroundColor Yellow
+        & $codeCmdPath --install-extension $extension
     }
+
+    if ($LaunchWhenDone) {
+        Write-Host "`nInstallation complete, starting $appName...`n`n" -ForegroundColor Green
+        & $codeCmdPath
+    }
+    else {
+        Write-Host "`nInstallation complete!`n`n" -ForegroundColor Green
+    }
+}
+finally {
+    $ProgressPreference = 'Continue'
+}
