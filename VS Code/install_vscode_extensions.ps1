28b1650bd95507da121d3c6db6939a9890c437f5
Jacques Gariépy <jacques.gariepy@outlook.com>
2023-09-16 20:44:12 -0400

    Update install_vscode_extensions.ps1

--- VS Code/install_vscode_extensions.ps1
+++ VS Code/install_vscode_extensions.ps1
@@ -1,87 +1,82 @@
 [CmdletBinding()]
 param(
-    [parameter()]
-    [ValidateSet(,"64-bit","32-bit")]
+    [Parameter()]
+    [ValidateSet("64-bit", "32-bit")]
     [string]$Architecture = "64-bit",
 
-    [parameter()]
-    [ValidateSet("stable","insider")]
-    
-    #stable ou insider
+    [Parameter()]
+    [ValidateSet("stable", "insider")]
     [string]$BuildEdition = "insider",
-    
+
     [Parameter()]
     [ValidateNotNull()]
     [string[]]$AdditionalExtensions = @(
-    "aaron-bond.better-comments",
-	"anteprimorac.html-end-tag-labels",
-	"DavidAnson.vscode-markdownlint",
-	"dbaeumer.vscode-eslint",
-	"eamodio.gitlens-insiders",
-	"EditorConfig.EditorConfig",
-	"eg2.vscode-npm-script",
-	"esbenp.prettier-vscode",
-	"formulahendry.auto-rename-tag",
-	"GitHub.copilot",
-	"GitHub.vscode-pull-request-github",
-	"johnpapa.vscode-peacock",
-	"johnpapa.winteriscoming",
-	"ms-dotnettools.csharp",
-	"ms-playwright.playwright",
-	"naumovs.color-highlight",
-	"PKief.material-icon-theme",
-	"rangav.vscode-thunder-client",
-	"VisualStudioExptTeam.vscodeintellicode",
-	"yzhang.markdown-all-in-one",
-	"ms-mssql.mssql",
-    	"esbenp.prettier-vscode",
-    	"ms-vscode.powershell",
-    	"ms-vscode.live-server"),
+        "aaron-bond.better-comments",
+        "anteprimorac.html-end-tag-labels",
+        "DavidAnson.vscode-markdownlint",
+        "dbaeumer.vscode-eslint",
+        "eamodio.gitlens-insiders",
+        "EditorConfig.EditorConfig",
+        "eg2.vscode-npm-script",
+        "esbenp.prettier-vscode",
+        "formulahendry.auto-rename-tag",
+        "GitHub.copilot",
+        "GitHub.vscode-pull-request-github",
+        "johnpapa.vscode-peacock",
+        "johnpapa.winteriscoming",
+        "ms-dotnettools.csharp",
+        "ms-playwright.playwright",
+        "naumovs.color-highlight",
+        "PKief.material-icon-theme",
+        "rangav.vscode-thunder-client",
+        "VisualStudioExptTeam.vscodeintellicode",
+        "yzhang.markdown-all-in-one",
+        "ms-mssql.mssql",
+        "ms-vscode.powershell",
+        "ms-vscode.live-server"
+    ),
 
     [switch]$LaunchWhenDone
 )
-	#path d'installation de vscode
-    $codePath = "C:\Users\jgariepy\AppData\Local\Programs\"
-    $bitVersion = "win32-x64"
 
-    try {
-        $ProgressPreference = 'SilentlyContinue'
+# Define the installation path for VSCode
+$codePath = "C:\Users\jgariepy\AppData\Local\Programs\"
 
-        #version des vscode
-		$buildEditions = @("stable", "insider")
+try {
+    $ProgressPreference = 'SilentlyContinue'
 
-        foreach ($buildEdition in $buildEditions) {
-
-            switch ($buildEdition) {
-                "Stable" {
-                    $codeCmdPath = "$codePath\Microsoft VS Code\bin\code.cmd"
-                    $appName = "Visual Studio Code ($($Architecture))"
-
-                    break;
-                }
-                "Insider" {
-                    $codeCmdPath = "$codePath\Microsoft VS Code Insiders\bin\code-insiders.cmd"
-                    $appName = "Visual Studio Code - Insiders Edition ($($Architecture))"
-                    break;
-                }
-            }
-            Write-Host "`nInstalling for $buildEdition..." -ForegroundColor Green
-            
-            $extensions = @("ms-vscode.PowerShell") + $AdditionalExtensions
-            foreach ($extension in $extensions) {
-                Write-Host "`nInstalling extension $extension" -ForegroundColor Yellow
-                & $codeCmdPath --install-extension $extension
-            }
-
-            if ($LaunchWhenDone) {
-                Write-Host "`nInstallation complete, starting $appName...`n`n" -ForegroundColor Green
-                & $codeCmdPath
-            }
-            else {
-                Write-Host "`nInstallation complete!`n`n" -ForegroundColor Green
-            }
+    # Determine the appropriate build edition and command path
+    switch ($BuildEdition) {
+        "stable" {
+            $codeCmdPath = Join-Path $codePath "Microsoft VS Code\bin\code.cmd"
+            $appName = "Visual Studio Code ($Architecture)"
+        }
+        "insider" {
+            $codeCmdPath = Join-Path $codePath "Microsoft VS Code Insiders\bin\code-insiders.cmd"
+            $appName = "Visual Studio Code - Insiders Edition ($Architecture)"
         }
     }
-    finally {
-        $ProgressPreference = 'Continue'
+
+    Write-Host "`nInstalling for $BuildEdition..." -ForegroundColor Green
+
+    # Install extensions
+    foreach ($extension in $AdditionalExtensions) {
+        Write-Host "`nInstalling extension $extension" -ForegroundColor Yellow
+        & $codeCmdPath --install-extension $extension
     }
+
+    # Launch VSCode if specified
+    if ($LaunchWhenDone) {
+        Write-Host "`nInstallation complete, starting $appName...`n`n" -ForegroundColor Green
+        & $codeCmdPath
+    }
+    else {
+        Write-Host "`nInstallation complete!`n`n" -ForegroundColor Green
+    }
+}
+catch {
+    Write-Error "An error occurred: $_"
+}
+finally {
+    $ProgressPreference = 'Continue'
+}
