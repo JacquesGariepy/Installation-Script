8ed84c9a41277f34d77038c833d903a83eb1ca88
Jacques Gariépy <jacques.gariepy@outlook.com>
2023-09-16 20:40:54 -0400

    Update install_applications.ps1

--- Applications/install_applications.ps1
+++ Applications/install_applications.ps1
@@ -10,20 +10,23 @@ winget install -e -h --id Google.Chrome.Beta --force --accept-source-agreements
 winget install -e -h --id Mozilla.Firefox --force --accept-source-agreements
 winget install -e -h --id Mozilla.Firefox.DeveloperEdition --force --accept-source-agreements
 winget install -e -h --id Microsoft.Edge.Beta --force --accept-source-agreements
+winget install -e -h --id BraveSoftware.BraveBrowser --force --accept-source-agreements  # Additional: Brave Browser
 
 # IDEs
 winget install -e -i --id Microsoft.VisualStudio.2022.Enterprise-Preview --force --accept-source-agreements
 winget install -e -h --id Microsoft.VisualStudioCode --force --accept-source-agreements
 winget install -e -h --id Microsoft.VisualStudioCode.Insiders --force --accept-source-agreements
 winget install -e -h --id Notepad++.Notepad++ --force --accept-source-agreements
+winget install -e -h --id JetBrains.PyCharm --force --accept-source-agreements  # Additional: PyCharm for Python Development
 
 # Frameworks
 winget install -e -h --id Microsoft.dotnet --force --accept-source-agreements
 winget install -e -h --id OpenJS.NodeJS --force --accept-source-agreements
 winget install -e -h --id Yarn.Yarn --force --accept-source-agreements
 
-# dev
+# Development
 winget install -e -h --id Python.Python.3 --force --accept-source-agreements
+winget install -e -h --id Anaconda.Anaconda3 --force --accept-source-agreements  # Additional: Anaconda for Data Science
 
 # Git
 winget install -e -i --id Git.Git --force --accept-source-agreements
@@ -31,10 +34,11 @@ winget install -e -h --id GitHub.GitLFS --force --accept-source-agreements
 winget install -e -h --id GitHub.cli --force --accept-source-agreements
 winget install -e -h --id Axosoft.GitKraken --force --accept-source-agreements
 
-#db
+# Database
 winget install -e -i --id Microsoft.AzureDataStudio.Insiders --force --accept-source-agreements
 winget install -e -i --id Microsoft.SQLServer.2019.Developer --force --accept-source-agreements
 winget install -e -i --id Microsoft.SQLServerManagementStudio --force --accept-source-agreements
+winget install -e -h --id MongoDB.MongoDB --force --accept-source-agreements  # Additional: MongoDB
 
 # Tools
 winget install -e -h --id Docker.DockerDesktop --force --accept-source-agreements
@@ -44,17 +48,9 @@ winget install -e -h --id JetBrains.ReSharper --force --accept-source-agreements
 winget install -e -h --id JetBrains.Rider --force --accept-source-agreements
 winget install -e -h --id JetBrains.WebStorm --force --accept-source-agreements
 winget install -e -h --id JetBrains.DataGrip --force --accept-source-agreements
+winget install -e -h --id Postman.Postman --force --accept-source-agreements  # Additional: Postman for API Testing
 
 # Azure
 winget install -e -h --id Microsoft.AzureDataStudio --force --accept-source-agreements
 winget install -e -h --id Microsoft.AzureStorageExplorer --force --accept-source-agreements
 winget install -e -h --id Microsoft.AzureCLI --force --accept-source-agreements
-winget install -e -h --id Microsoft.AzureCosmosEmulator --force --accept-source-agreements
-winget install -e -h --id Microsoft.AzureStorageEmulator --force --accept-source-agreements
-
-# Utilities
-winget install -e -h --id 7zip.7zip --force --accept-source-agreements
-winget install -e -h --id Adobe.Acrobat.Reader.64-bit --force --accept-source-agreements
-winget install -e -h --id Microsoft.PowerToys --force --accept-source-agreements
-winget install -e -h --id WinSCP.WinSCP --force --accept-source-agreements
-winget install -e -h --id Greenshot.Greenshot --accept-source-agreements
