310bd2688305947d8d2dedb3fd884af0fb110802
Jacques Gariépy <jacques.gariepy@outlook.com>
2023-09-16 20:33:17 -0400

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,37 +1,21 @@
-set-executionpolicy unrestricted
-Write-Host "Autoriser l’exécution de scripts PowerShell. Si powershell avait une restriction préalable, veuillez relancer ce script." -ForegroundColor Yellow
+# Set the execution policy
+Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
+Write-Host "Autoriser l’exécution de scripts PowerShell. Si PowerShell avait une restriction préalable, veuillez relancer ce script." -ForegroundColor Yellow
 
-Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
-Write-Host "Installation" -ForegroundColor Yellow
-
-$confirmation = Read-Host "Installation applications (y/n)"
-if ($confirmation -eq 'y') {
-  &"$PSScriptRoot\Applications\install_applications.ps1"  
+# Function to prompt and run scripts
+function RunScript($promptMessage, $scriptPath) {
+    $confirmation = Read-Host $promptMessage
+    if ($confirmation -eq 'y') {
+        &"$PSScriptRoot\$scriptPath"
+    }
 }
 
-$confirmation = Read-Host "Installation Visual Studio 2022 Extensions (y/n)"
-if ($confirmation -eq 'y') {
-  &"$PSScriptRoot\VisualStudio2022\install_visualstudio_extensions.ps1"  
-}
+# Call the function for each script
+RunScript "Installation applications (y/n)" "Applications\install_applications.ps1"
+RunScript "Installation Visual Studio 2022 Extensions (y/n)" "VisualStudio2022\install_visualstudio_extensions.ps1"
+RunScript "Installation VSCode Extensions (y/n)" "VS Code\install_vscode_extensions.ps1"
+RunScript "Installation VSCode Angular Extensions (y/n)" "VS Code\install_vscode_angular_extensions.ps1"
+RunScript "Installation VSCode React & React Native Extensions (y/n)" "VS Code\install_vscode_react_extensions.ps1"
+RunScript "Installation Chrome Extensions (y/n)" "Chrome\install_chrome_extensions.ps1"
 
-$confirmation = Read-Host "Installation VSCode Extensions (y/n)"
-if ($confirmation -eq 'y') {
-  &"$PSScriptRoot\VS Code\install_vscode_extensions.ps1"
-}
-
-$confirmation = Read-Host "Installation VSCode Angular Extensions (y/n)"
-if ($confirmation -eq 'y') {
-  &"$PSScriptRoot\VS Code\install_vscode_angular_extensions.ps1"
-}
-
-$confirmation = Read-Host "Installation VSCode React & React Native Extensions (y/n)"
-if ($confirmation -eq 'y') {
-  &"$PSScriptRoot\VS Code\install_vscode_react_extensions.ps1"
-}
-
-$confirmation = Read-Host "Installation Chrome Extensions (y/n)"
-if ($confirmation -eq 'y') {
-  &"$PSScriptRoot\Chrome\install_chrome_extensions.ps1"
-}
-
-Write-Host "Fin de l'nstallation" -ForegroundColor Yellow
+Write-Host "Fin de l'installation" -ForegroundColor Yellow
