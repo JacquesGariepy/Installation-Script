b1bb59d8ab618c75186308b2d43d28dcb32e56a4
Jacques Gariépy <jacques.gariepy@outlook.com>
2023-10-17 21:30:45 -0400

    Update install_applications.ps1

--- Applications/install_applications.ps1
+++ Applications/install_applications.ps1
@@ -31,8 +31,8 @@ winget install -e -h --id Mozilla.Firefox --force --accept-source-agreements
 winget install -e -h --id Mozilla.Firefox.DeveloperEdition --force --accept-source-agreements
 winget install -e -h --id Microsoft.Edge.Beta --force --accept-source-agreements
 winget install -e -h --id BraveSoftware.BraveBrowser --force --accept-source-agreements  # Additional: Brave Browser
-winget install -e -h --id=Brave.Brave.Nightly --force --accept-source-agreements 
-winget install -e -h --id=TorProject.TorBrowser --force --accept-source-agreements
+winget install -e -h --id Brave.Brave.Nightly --force --accept-source-agreements 
+winget install -e -h --id TorProject.TorBrowser --force --accept-source-agreements
 
 # IDEs
 winget install -e -i --id Microsoft.VisualStudio.2022.Enterprise-Preview --force --accept-source-agreements
@@ -51,16 +51,18 @@ winget install -e -h --id Yarn.Yarn --force --accept-source-agreements
 # Development
 winget install -e -h --id Python.Python.3 --force --accept-source-agreements
 winget install -e -h --id Anaconda.Anaconda3 --force --accept-source-agreements
-# winget install -e -h --id --id=Anaconda.Miniconda3 --force --accept-source-agreements #miniconda or anaconda
-winget install -e -h --id=Microsoft.WindowsTerminal --force --accept-source-agreements
-winget install -e -h --id=Microsoft.WingetCreate  --force --accept-source-agreements
+# winget install -e -h --id Anaconda.Miniconda3 --force --accept-source-agreements #miniconda or anaconda
+winget install -e -h --id Microsoft.WindowsTerminal --force --accept-source-agreements
+winget install -e -h --id Microsoft.WingetCreate --force --accept-source-agreements
+winget install -e -h --id GitTools.GitVersion --force --accept-source-agreements
 
 # Git
 winget install -e -i --id Git.Git --force --accept-source-agreements
 winget install -e -h --id GitHub.GitLFS --force --accept-source-agreements
 winget install -e -h --id GitHub.cli --force --accept-source-agreements
-winget install -e -h --id=GitHub.GitHubDesktop  --force --accept-source-agreements
+winget install -e -h --id GitHub.GitHubDesktop.Beta  --force --accept-source-agreements 
 winget install -e -h --id Axosoft.GitKraken --force --accept-source-agreements
+winget install -e -h --id StefHeyenrath.GitHubReleaseNotes --force --accept-source-agreements
 
 # Database
 winget install -e -i --id Microsoft.AzureDataStudio.Insiders --force --accept-source-agreements
@@ -68,7 +70,7 @@ winget install -e -i --id Microsoft.SQLServer.2019.Developer --force --accept-so
 winget install -e -i --id Microsoft.SQLServerManagementStudio --force --accept-source-agreements
 winget install -e -h --id MongoDB.MongoDB --force --accept-source-agreements  # Additional: MongoDB
 winget install -e -h --id AllroundAutomations.PLSQLDeveloper -force --accept-source-agreements
-winget install -e -h --id=PostgreSQL.PostgreSQL -force --accept-source-agreements
+winget install -e -h --id PostgreSQL.PostgreSQL -force --accept-source-agreements
 
 # Tools
 winget install -e -h --id Docker.DockerDesktop --force --accept-source-agreements
@@ -80,13 +82,14 @@ winget install -e -h --id JetBrains.WebStorm --force --accept-source-agreements
 winget install -e -h --id JetBrains.DataGrip --force --accept-source-agreements
 winget install -e -h --id Postman.Postman --force --accept-source-agreements  # Additional: Postman for API Testing
 winget install -e -h --id SmartBear.SoapUI --force --accept-source-agreements
-winget install -e -h --id=PuTTY.PuTTY --force --accept-source-agreements
-winget install -e -h --id=RARLab.WinRAR --force --accept-source-agreements
-winget install -e -h --id=M2Team.NanaZip --force --accept-source-agreements
-winget install -e -h --id=TorProject.TorBrowser --force --accept-source-agreements
-winget install -e -h --id=Obsidian.Obsidian --force --accept-source-agreements
-winget install -e -h --id=SomePythonThings.WingetUIStore --force --accept-source-agreements
-winget install -e -h --id=HandyOrg.HandyWinget-GUI --force --accept-source-agreements
+winget install -e -h --id PuTTY.PuTTY --force --accept-source-agreements
+winget install -e -h --id RARLab.WinRAR --force --accept-source-agreements
+winget install -e -h --id M2Team.NanaZip --force --accept-source-agreements
+winget install -e -h --id TorProject.TorBrowser --force --accept-source-agreements
+winget install -e -h --id Obsidian.Obsidian --force --accept-source-agreements
+winget install -e -h --id SomePythonThings.WingetUIStore --force --accept-source-agreements
+winget install -e -h --id HandyOrg.HandyWinget-GUI --force --accept-source-agreements
+winget install -e -h --id Microsoft.PowerToys --force --accept-source-agreements
 
 # Azure
 winget install -e -h --id Microsoft.AzureDataStudio --force --accept-source-agreements
@@ -97,15 +100,15 @@ winget install -e -h --id Microsoft.AzureCLI --force --accept-source-agreements
 winget install -e -h --id Oracle.VirtualBox --force --accept-source-agreements 
 
 #Organisation
-winget install -e -h --id=Notion.Notion --force --accept-source-agreements 
-winget install -e -h --id=Discord.Discord --force --accept-source-agreements 
+winget install -e -h --id Notion.Notion --force --accept-source-agreements 
+winget install -e -h --id Discord.Discord --force --accept-source-agreements 
 
 #Video
-winget install -e -h --id=OBSProject.OBSStudio.Pre-release --force --accept-source-agreements
+winget install -e -h --id OBSProject.OBSStudio.Pre-release --force --accept-source-agreements
 
 #hardware
-winget install -e -h --id=CPUID.CPU-Z --force --accept-source-agreements
+winget install -e -h --id CPUID.CPU-Z --force --accept-source-agreements
 
 #secure
-winget install -e -h --id=Bitwarden.Bitwarden --force --accept-source-agreements
-winget install -e -h --id=PrestonN.FreeTube --force --accept-source-agreements
+winget install -e -h --id Bitwarden.Bitwarden --force --accept-source-agreements
+winget install -e -h --id PrestonN.FreeTube --force --accept-source-agreements
