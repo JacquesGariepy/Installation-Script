cb8a79f1cf8015bc8dbb0a6ce603af2c2d841766
Jacques Gariépy <jacques.gariepy@outlook.com>
2023-11-03 16:06:18 -0400

    Update install_python_src.ps1

--- Src/install_python_src.ps1
+++ Src/install_python_src.ps1
@@ -1,8 +1,15 @@
 # Liste des projets GitHub à cloner
 $githubProjects = @(
     "https://github.com/JacquesGariepy/Installation-Script.git",
-    "https://github.com/votre-utilisateur/votre-projet.git",
+    #"https://github.com/votre-utilisateur/votre-projet.git",
     # Ajoutez d'autres liens de projets GitHub ici
+)
+
+# Liste des packages pip à installer
+$pipPackages = @(
+    "apache-airflow[celery]==2.7.2 --constraint https://raw.githubusercontent.com/apache/airflow/constraints-2.7.2/constraints-3.8.txt",
+    #"nom-du-package==version",
+    # Ajoutez d'autres packages pip ici
 )
 
 # Demander à l'utilisateur de fournir le nom de l'environnement Anaconda (ou utiliser "base" par défaut)
@@ -27,3 +34,4 @@ foreach ($pipPackage in $pipPackages) {
 }
 
 Write-Host "Toutes les opérations ont été effectuées avec succès."
+
